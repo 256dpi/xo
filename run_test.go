18baf2ed7fd7be34732bf1bfd992cb6cff4aa1c4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-06-03 11:00:04 +0200

    improved test

--- run_test.go
+++ run_test.go
@@ -4,35 +4,95 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRun(t *testing.T) {
-	err := Run(nil, func(ctx *Context) error {
-		ctx.Tag("tag", 42)
-		ctx.Log("bar")
-		return nil
-	})
-	assert.NoError(t, err)
+	Trap(func(mock *Mock) {
+		err := Run(nil, func(ctx *Context) error {
+			ctx.Tag("tag", 42)
+			ctx.Log("bar")
+			return nil
+		})
+		assert.NoError(t, err)
 
-	err = Run(nil, func(ctx *Context) error {
-		ctx.Tag("tag", 42)
-		ctx.Log("bar")
-		return F("error")
-	})
-	assert.Error(t, err)
-	assert.Equal(t, "error", err.Error())
-	assert.Equal(t, "xo.TestRun: error", fmt.Sprintf("%v", err))
+		err = Run(nil, func(ctx *Context) error {
+			ctx.Tag("tag", 42)
+			ctx.Log("bar")
+			return F("error")
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "error", err.Error())
+		assert.Equal(t, "xo.TestRun.func1: error", fmt.Sprintf("%v", err))
 
-	err = Run(nil, func(ctx *Context) error {
-		ctx.Tag("tag", 42)
-		ctx.Log("bar")
-		panic("error")
+		err = Run(nil, func(ctx *Context) error {
+			ctx.Tag("tag", 42)
+			ctx.Log("bar")
+			panic("error")
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "PANIC: error", err.Error())
+		assert.Equal(t, "xo.TestRun.func1: PANIC: error", fmt.Sprintf("%v", err))
+
+		assert.Equal(t, []MemorySpan{
+			{
+				Name: "xo.TestRun.func1",
+				Attributes: M{
+					"tag": int64(42),
+				},
+				Events: []MemorySpanEvent{
+					{
+						Name: "log",
+						Attributes: M{
+							"message": "bar",
+						},
+					},
+				},
+			},
+			{
+				Name:       "xo.TestRun.func1",
+				Attributes: M{"tag": int64(42)},
+				Events: []MemorySpanEvent{
+					{
+						Name: "log",
+						Attributes: M{
+							"message": "bar",
+						},
+					},
+					{
+						Name: "error",
+						Attributes: M{
+							"error.message": "error",
+							"error.type":    "*xo.Err",
+						},
+					},
+				},
+			},
+			{
+				Name: "xo.TestRun.func1",
+				Attributes: M{
+					"tag": int64(42),
+				},
+				Events: []MemorySpanEvent{
+					{
+						Name: "log",
+						Attributes: M{
+							"message": "bar",
+						},
+					},
+					{
+						Name: "error",
+						Attributes: M{
+							"error.message": "PANIC: error",
+							"error.type":    "*xo.Err",
+						},
+					},
+				},
+			},
+		}, mock.ReducedSpans(10*time.Millisecond))
 	})
-	assert.Error(t, err)
-	assert.Equal(t, "PANIC: error", err.Error())
-	assert.Equal(t, "xo.TestRun: PANIC: error", fmt.Sprintf("%v", err))
 }
 
 func BenchmarkRun(b *testing.B) {

47ffba9d7db17e604e5f593e9b5d4a801519b836
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-10-20 14:00:49 +0200

    added advanced env loading

--- env.go
+++ env.go
@@ -1,6 +1,9 @@
 package xo
 
-import "os"
+import (
+	"io/ioutil"
+	"os"
+)
 
 // Get will get the specified environment variable and fallback to the specified
 // value if it is missing or empty.
@@ -12,4 +15,54 @@ func Get(key, fallback string) string {
 	}
 
 	return fallback
+}
+
+// Devel is true when the program runs in development mode.
+var Devel = os.Getenv("DEVEL") == "true"
+
+// Var defines an environment variable.
+type Var struct {
+	// Then name of the variable e.g. "SOME_VAR".
+	Name string
+
+	// Whether providing a value is required.
+	Require bool
+
+	// The main default value.
+	Main string
+
+	// The development default value.
+	Devel string
+
+	// Whether the value names a file that should be read.
+	File bool
+}
+
+// Load will return the value of the provided environment variable.
+func Load(v Var) string {
+	// get variable
+	val := os.Getenv(v.Name)
+	if val == "" {
+		if Devel {
+			val = v.Devel
+		} else {
+			val = v.Main
+		}
+	}
+
+	// check require
+	if val == "" && v.Require {
+		Panic(F("missing variable " + v.Name))
+	}
+
+	// load file
+	if v.File {
+		buf, err := ioutil.ReadFile(val)
+		if err != nil {
+			Panic(WF(err, "unable to load file"))
+		}
+		val = string(buf)
+	}
+
+	return val
 }
--- env_test.go
+++ env_test.go
@@ -14,3 +14,29 @@ func TestGet(t *testing.T) {
 	assert.Equal(t, "baz", Get("FOO", ""))
 	assert.Equal(t, "baz", Get("FOO", "bar"))
 }
+
+func TestLoad(t *testing.T) {
+	assert.Equal(t, "", Load(Var{Name: "foo", Main: "", Devel: ""}))
+	assert.Equal(t, "main", Load(Var{Name: "foo", Main: "main", Devel: "devel"}))
+	assert.NoError(t, os.Setenv("foo", "baz"))
+	assert.Equal(t, "baz", Load(Var{Name: "foo", Main: "main", Devel: "devel"}))
+
+	Devel = true
+	assert.NoError(t, os.Setenv("foo", ""))
+
+	assert.Equal(t, "", Load(Var{Name: "foo", Main: "", Devel: ""}))
+	assert.Equal(t, "devel", Load(Var{Name: "foo", Main: "main", Devel: "devel"}))
+	assert.NoError(t, os.Setenv("foo", "baz"))
+	assert.Equal(t, "baz", Load(Var{Name: "foo", Main: "main", Devel: "devel"}))
+
+	assert.Panics(t, func() {
+		Load(Var{Name: "bar", Require: true})
+	})
+
+	Devel = false
+
+	assert.NotEmpty(t, Load(Var{Name: "file", Main: "./env.go", File: true}))
+	assert.Panics(t, func() {
+		assert.NotEmpty(t, Load(Var{Name: "file", Main: "./foo.go", File: true}))
+	})
+}
